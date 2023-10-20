adc3ef97e3c1e8a57f5998a45575a051bf7f256e
Pavel Kirilin <win10@list.ru>
2023-10-20 19:42:46 +0400

    AAA.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- argo_testing/__main__.py
+++ argo_testing/__main__.py
@@ -1,9 +1,36 @@
 import time
+from typer import Typer
+from aiohttp import web
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
-def main():
-    print("Everything is good.")
-    time.sleep(1)
+cli = Typer()
+
+router = web.RouteTableDef()
+
+
+@router.get("/")
+async def index(request: web.Request) -> web.Response:
+    return web.Response(text="Hello World")
+
+
+app = web.Application(logger=print)
+app.router.add_routes(router)
+
+
+@cli.command()
+def server():
+    web.run_app(app, host="0.0.0.0", port=8000, access_log=logging)
+
+
+@cli.command()
+def migrate() -> None:
+    print("Migrating")
+    time.sleep(4)
+    print("Migrated")
+
 
 if __name__ == "__main__":
-    main()
+    cli()
