72193bd34f6a405bf17052dab17b8b5df8b4e87c
Pavel Kirilin <win10@list.ru>
2023-10-21 00:21:28 +0400

    Bump version to 1.0.8

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 poetry version "${1}"
 docker build -t "registry.localhost:5000/argo-testing:${1}" .
 docker push "registry.localhost:5000/argo-testing:${1}"
-sed -i "s/tag:.*$/tag: ${1}/" deploy/envs/in-cluster.yaml
+yq -iy ".image.tag = \"${1}\"" deploy/envs/in-cluster.yaml
 git add .
 git commit -m "Bump version to ${1}"
